aef2627a789403d9e8761251b174ff2af9df8210
selemonjr <selemonjr@gmail.com>
2022-01-17 14:35:58 +0300

    useFetch Composable

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="app">
+  <div>
+     <Fetch/>
+  </div>
+</div>
+</template>
+
+<script>
+import Fetch from './components/Fetch.vue';
+export default {
+  name: 'App',
+  components: {
+    Fetch
+  }
+}
+</script>
+
+<style>
+* {
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+}
+body {
+  height:100vh;
+  width:100%;
+  font-family:sans-serif;
+  padding:1rem;
+}
+</style>
---
+++ src/components/Fetch.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="app">
+       <p v-if="loading">Loading....</p>
+      <div v-else>
+            <div  class="flex" v-if="response && response.length > 0">
+            <div v-for="item in response" :key="item.id">
+            <p>{{item.title.substring(0,5)}}</p>
+            <img :src="item.url" alt="">
+        </div>
+      </div>
+        </div>
+    </div>
+</template>
+<script setup>
+ import { useFetch } from '@vueuse/core';
+ const url = "https://jsonplaceholder.typicode.com/photos";
+ const {isFetching: loading, data:response} = useFetch(url).json();
+</script>
+<style scoped>
+.flex {
+    display:flex;
+    width:100%;
+    flex-wrap:wrap;
+}
+p {
+    text-align:center;
+}
+img {
+    width:6rem;
+    height:6rem;
+    margin:1rem;
+}
+</style>
